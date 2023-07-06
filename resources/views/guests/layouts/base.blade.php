a6b52395ba20c3418002355154360d349658c999
LambertoNeri <neri.lamberto91@gmail.com>
2023-07-06 17:05:40 +0200

    add exercise 06-07-2023

--- resources/views/guests/layouts/base.blade.php
+++ resources/views/guests/layouts/base.blade.php
@@ -4,14 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Laravel Auth</title>
     @vite('resources/js/app.js')
 </head>
 <body>
-    @include('admin.includes.header')
-    <main>
-        @yield('contents')
-    </main>
-    @include('admin.includes.footer')
+    @include('guests.includes.header')
+
+
+
+    <div class="container">
+        <main>
+            @yield('contents')
+        </main>
+    </div>
+
+
+    @include('guests.includes.footer')
+    
 </body>
 </html>
